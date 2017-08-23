cf857050887de4adfdff61ccdd09750b3b8be198
Dabin <dorothy9795@gmail.com>
2017-08-23 09:53:19 +0900

    no message

---
+++ src/AsepriteScanner.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SimpleAsepriteToSpine
+{
+    class AsepriteScanner
+    {
+        private string asepriteParentDire; //Aseprite.exe 파일 경로
+        private string aseFilePath; //스캔할 .ase 파일 경로 (이름을 제외한)
+        private string aseFileName; //스캔할 .ase 파일 이름
+
+        public AsepriteScanner()
+        {
+            JObject jSpineObj = new JObject();
+            jSpineObj.Add("skins", new JObject(new JProperty("default", new JObject())));
+            string aa = jSpineObj.ToString();
+
+
+            JObject jo = (JObject)jSpineObj["skins"]["default"];
+            jo.Add("test", "dd");
+
+            Console.WriteLine(aa);
+        }
+
+        public bool FindAsepriteByDialog()
+        {
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+            fileDialog.DefaultExt = ".exe";
+            fileDialog.Filter = "Aseprite File|Aseprite.exe";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                SetAsepriteParentDire(fileDialog.FileName);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public bool FindAseFileByDialog()
+        {
+            Microsoft.Win32.OpenFileDialog fileDialog = new Microsoft.Win32.OpenFileDialog();
+            fileDialog.DefaultExt = ".exe";
+            fileDialog.Filter = "Aseprite (*.ase)|*.ase";
+
+            if (fileDialog.ShowDialog() == true)
+            {
+                SetAseFilePath(fileDialog.FileName.Replace(fileDialog.SafeFileName, ""));
+                SetAseFileName(fileDialog.SafeFileName.Replace(".ase", ""));
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public void ExportJsonWithLayersPng()
+        {
+            ProcessStartInfo proStartInfo = new ProcessStartInfo();
+            Process process = new Process();
+            proStartInfo.FileName = "cmd";
+            proStartInfo.WorkingDirectory = @"C:\";
+            proStartInfo.RedirectStandardOutput = true;
+            proStartInfo.RedirectStandardInput = true;
+            proStartInfo.RedirectStandardError = true;
+            proStartInfo.UseShellExecute = false;
+            proStartInfo.CreateNoWindow = true;
+            process.StartInfo = proStartInfo;
+            process.Start();
+
+            //Aseprite 위치로 이동
+            process.StandardInput.Write(@"cd " + asepriteParentDire + Environment.NewLine);
+
+            //json Array 타입으로 추출, 레이어 각각을 모두 png 파일로 추출
+            string writeString = "aseprite.exe -b"
+                + " --split-layers " + aseFilePath + aseFileName + ".ase"
+                + " --filename-format {path}\\{layer}.{extension}"
+                + " --trim"
+                + " --save-as "+ aseFilePath + ".png"
+                + " --sheet " + aseFileName + ".png"
+                + " --format json-array --data " + aseFilePath + aseFileName + "_Aseprite.json"
+                + Environment.NewLine;
+
+            process.StandardInput.Write(writeString);
+            process.StandardInput.Close();
+
+            string result = process.StandardOutput.ReadToEnd();
+            process.WaitForExit();
+            process.Close();
+
+            //뽑힌 Aseprite json 파일을 Spine 에서 사용가능하도록 컨버팅
+            ExportJsonForUseInSpine(aseFilePath + aseFileName + "_Aseprite.json");
+        }
+
+        public void ExportJsonForUseInSpine(string jsonFilePath)
+        {
+            string jsonString = System.IO.File.ReadAllText(jsonFilePath);
+
+            JObject jAspObj = JObject.Parse(jsonString);
+            JArray jfreams = (JArray)jAspObj["frames"];
+
+            JObject jSpineObj = new JObject();
+            jSpineObj.Add("skeleton", new JObject(new JProperty("images", aseFilePath)));
+            jSpineObj.Add("bones", new JArray(new JObject(new JProperty("name", "root"))));
+            jSpineObj.Add("slots", new JArray());
+            jSpineObj.Add("skins", new JObject(new JProperty("default", new JObject())));
+
+            for (int i=0; i< jfreams.Count; i++)
+            {
+                //※Aseprite 에서 Trim 설정으로 자른 이미지의 좌표정보는 Anchor가 왼쪽 위 기준으로 잡혀있더라.
+
+                string filePath = (string)jfreams[i]["filename"];
+                string fileName = filePath.Replace(aseFilePath, "").Replace(".ase", "");
+                int imgHeight = (int)jfreams[i]["sourceSize"]["h"];
+                int imgWidth = (int)jfreams[i]["sourceSize"]["w"];
+                int x = (int)jfreams[i]["spriteSourceSize"]["x"];
+                int y = (int)jfreams[i]["spriteSourceSize"]["y"];
+                int w = (int)jfreams[i]["spriteSourceSize"]["w"];
+                int h = (int)jfreams[i]["spriteSourceSize"]["h"];
+
+                JObject jslot = new JObject();
+                jslot.Add(new JProperty("name", fileName));
+                jslot.Add(new JProperty("bone", "root"));
+                jslot.Add(new JProperty("attachment", fileName));
+                JArray jslots = (JArray)jSpineObj["slots"];
+                jslots.Add(jslot);
+
+                JObject jImgInfo = new JObject();
+                jImgInfo.Add(new JProperty("x", x + w/2));
+                jImgInfo.Add(new JProperty("y", imgHeight - (y + h/2)));
+                jImgInfo.Add(new JProperty("width", w));
+                jImgInfo.Add(new JProperty("height", h));
+
+                JObject jskins = (JObject)jSpineObj["skins"]["default"];
+                jskins.Add(fileName, new JObject(new JProperty(fileName, jImgInfo)));
+            }
+
+            jSpineObj.Add("animations", new JObject(new JProperty("animation", new JObject())));
+
+            string spineJsonString = jSpineObj.ToString();
+            System.IO.File.WriteAllText(aseFilePath + aseFileName + "_Spine.json", spineJsonString);
+
+        }
+
+        /*----------------
+        // Getter&Setter
+        -----------------*/
+
+        public string GetAsepriteParentDire()
+        {
+            return asepriteParentDire;
+        }
+        public string GetAseFilePath()
+        {
+            return aseFilePath;
+        }
+        public string GetAseFileName()
+        {
+            return aseFileName;
+        }
+
+        public void SetAsepriteParentDire(string asepritePath)
+        {
+            asepriteParentDire = asepritePath.Replace("Aseprite.exe", "");
+        }
+
+        public void SetAseFilePath(string aseFilePath)
+        {
+            this.aseFilePath = aseFilePath;
+        }
+
+        public void SetAseFileName(string aseFileName)
+        {
+            this.aseFileName = aseFileName;
+        }
+    }
+}
---
+++ src/MainWindow.xaml.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Runtime.InteropServices;
+using System.IO;
+using System.Reflection;
+
+namespace SimpleAsepriteToSpine
+{
+    /// <summary>
+    /// MainWindow.xaml에 대한 상호 작용 논리
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private AsepriteScanner scanner;
+        private bool isFindedAseprite = false;
+        private string asepritePath = "";
+
+        [DllImport("kernel32")]
+        private static extern long WritePrivateProfileString(string section, string key, string val, string filePath);
+        [DllImport("kernel32")]
+        private static extern int GetPrivateProfileString(string section, string key, string def, StringBuilder retVal, int size, string filePath);
+
+        public MainWindow()
+        {
+            AppDomain.CurrentDomain.AssemblyResolve += new ResolveEventHandler(OnResolveAssembly);
+
+            scanner = new AsepriteScanner();
+            InitializeComponent();
+            InitINIFile();
+        }
+
+        private void Button_Click_FindAseprite(object sender, RoutedEventArgs e)
+        {
+            if (scanner.FindAsepriteByDialog())
+            {
+                tbAsepriteFilePath.Text = scanner.GetAsepriteParentDire() + "Aseprite.exe";
+                SaveAsepritePathINIFile(tbAsepriteFilePath.Text);
+            }
+        }
+
+        private void Button_Click_FindJson(object sender, RoutedEventArgs e)
+        {
+            if (isFindedAseprite)
+            {
+                if (scanner.FindAseFileByDialog())
+                {
+                    scanner.ExportJsonWithLayersPng();
+
+                    MessageBox.Show("변환 성공");
+                }
+            }
+            else
+            {
+                MessageBox.Show("Aseprite.exe 경로를 설정하세요");
+            }
+        }
+
+        private void InitINIFile()
+        {
+            if (File.Exists(AppDomain.CurrentDomain.BaseDirectory + "AsepriteToSpine.ini"))
+            {
+                StringBuilder path = new StringBuilder();
+                GetPrivateProfileString("Config", "AsepritePath", "NONE", path, 100, AppDomain.CurrentDomain.BaseDirectory + "AsepriteToSpine.ini");
+
+                if (path.ToString() != "NONE")
+                {
+                    isFindedAseprite = true;
+                    asepritePath = path.ToString();
+                    SetAsepritePathTextBox(asepritePath);
+                }
+            }
+        }
+
+        private void SetAsepritePathTextBox(string asepritePath)
+        {
+            tbAsepriteFilePath.Text = asepritePath;
+            tbAsepriteFilePath.Select(tbAsepriteFilePath.Text.Length, 0);
+        }
+
+        private void SaveAsepritePathINIFile(string asepritePath)
+        {
+            SetAsepritePathTextBox(asepritePath);
+            WritePrivateProfileString("Config", "AsepritePath", asepritePath, AppDomain.CurrentDomain.BaseDirectory + "AsepriteToSpine.ini");
+            isFindedAseprite = true;
+            this.asepritePath = asepritePath;
+        }
+
+        //단일 exe 실행을 위한 함수
+        private static Assembly OnResolveAssembly(object sender, ResolveEventArgs args)
+        {
+            //어셈블리가 .exe파일과 같은 폴더에 있으면 이 메서드는 호출되지 않습니다.
+            Assembly thisAssembly = Assembly.GetExecutingAssembly();
+
+            AssemblyName assemblyName = new AssemblyName(args.Name);
+            string name = args.Name.Substring(0, args.Name.IndexOf(',')) + ".dll";
+ 
+            var resources = thisAssembly.GetManifestResourceNames().Where(s => s.EndsWith(name));
+            if (resources.Any())
+            {
+                var resourceName = resources.First();
+                using (Stream stream = thisAssembly.GetManifestResourceStream(resourceName))
+                {
+                    if (stream == null)
+                        return null;
+                    var block = new byte[stream.Length];
+                    stream.Read(block, 0, block.Length);
+                    return Assembly.Load(block);
+                }
+             }
+            return null;
+        }
+    }
+}
